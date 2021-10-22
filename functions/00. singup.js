8dd5b605ec8f27560f7bff1c03febcb6dc30b29a
Prasetiyo <84963363+prasetiyo-2020@users.noreply.github.com>
2021-10-22 09:04:22 +0700

    Create 00. singup.js

---
+++ functions/00. singup.js
@@ -0,0 +1,66 @@
+/*
+  Important : Try code result with FAKE DATA
+  https://blog-prasetiyo.herokuapp.com/signup
+*/
+
+app.post('/signup',
+    (req, res, next) => {
+        console.log('Pemeriksaan nilai input kosong');
+        // const { username, email, password } = req.body;
+        const username = req.body.username;
+        const email = req.body.email;
+        const password = req.body.password;
+        const errors = [];
+        if (username === '') {
+            errors.push('Nama Pengguna kosong');
+        }
+        if (email === '') {
+            errors.push('Email kosong');
+        }
+        if (password === '') {
+            errors.push('Kata Sandi kosong');
+        }
+        if (errors.length > 0) {
+            res.render('signup.ejs', {
+                errors: errors
+            });
+        } else {
+            next();
+        }
+    },
+    (req, res, next) => {
+        console.log('Pemeriksaan email duplikat');
+        const email = req.body.email;
+        const errors = [];
+        connection.query(
+            'SELECT * FROM users WHERE email = ?',
+            [email],
+            (error, results) => {
+                if (results.length > 0) {
+                    errors.push('Gagal mendaftarkan pengguna');
+                    res.render('signup.ejs', {
+                        errors: errors
+                    });
+                } else {
+                    next();
+                }
+            }
+        );
+    },
+    (req, res) => {
+        console.log('Pendaftaran');
+        // const { username, email, password } = req.body;
+        const username = req.body.username;
+        const email = req.body.email;
+        const password = req.body.password;
+        connection.query(
+            'INSERT INTO users (username, email, password) VALUES (?, ?, ?)',
+            [username, email, password],
+            (error, results) => {
+                req.session.userId = results.insertId;
+                req.session.username = username;
+                res.redirect('/list');
+            }
+        );
+    }
+);
